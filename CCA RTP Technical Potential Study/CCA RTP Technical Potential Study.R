679ab3366b055994ba6b7dfb894fe8a9c8fd46b6
RyanCMann <ryancmann@mac.com>
2020-02-01 14:04:32 -0800

    Clean CAISO RT5M Data

--- CCA RTP Technical Potential Study/CCA RTP Technical Potential Study.R
+++ CCA RTP Technical Potential Study/CCA RTP Technical Potential Study.R
@@ -15,10 +15,34 @@ Home_WD <- getwd()
 
 
 #### Load and Clean CAISO RT5M Wholesale Price Data ####
+# Data is for PG&E DLAP, Real-Time 5-Minute Market
 
+setwd(file.path(Home_WD, "CAISO RT5M Price Data"))
 
+CAISO_RT5M_Files <- list.files(pattern = ".csv")
 
+Raw_CAISO_RT5M_Joined <- data.frame(INTERVALSTARTTIME_GMT = character(), VALUE = numeric(), stringsAsFactors = F)
+  
+for(CAISO_RT5M_File in CAISO_RT5M_Files){
+  
+  Raw_CAISO_RT5M_Single <- read.csv(CAISO_RT5M_File) %>%
+    filter(XML_DATA_ITEM == "LMP_PRC") %>%
+    select(INTERVALSTARTTIME_GMT, VALUE)
+  
+  Raw_CAISO_RT5M_Joined <- rbind(Raw_CAISO_RT5M_Joined, Raw_CAISO_RT5M_Single)
+  
+}
 
+rm(Raw_CAISO_RT5M_Single, CAISO_RT5M_File, CAISO_RT5M_Files)
+
+Clean_CAISO_RT5M_Joined <- Raw_CAISO_RT5M_Joined %>%
+  mutate(dttm = as.POSIXct(gsub("T", " ", substr(INTERVALSTARTTIME_GMT, 1, 16)), tz = "UTC")) %>%
+  mutate(dttm = with_tz(dttm, tz = "America/Los_Angeles")) %>%
+  mutate(LMP_RT5M = VALUE/1000) %>% # Convert from $/MWh to $/kWh
+  select(dttm, LMP_RT5M) %>%
+  arrange(dttm)
+
+rm(Raw_CAISO_RT5M_Joined)
 
 
 #### Load and Clean WattTime SGIP Historical GHG Data ####
